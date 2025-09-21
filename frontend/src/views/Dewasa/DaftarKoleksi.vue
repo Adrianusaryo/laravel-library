f594d438e5104f2f107ffce8230d597fb7d76bc8
Adrianus Aryo <wicaksonoadrianus5@gmail.com>
2025-09-21 13:45:59 +0700

    fix url

--- frontend/src/views/Dewasa/DaftarKoleksi.vue
+++ frontend/src/views/Dewasa/DaftarKoleksi.vue
@@ -205,7 +205,7 @@ export default {
 
             axios
                 .get(
-                    `https://laravel-library-production.up.railway.app/api/Buku/KoleksiBuku?usia=dewasa&page=${page}&per_page=${this.perPage}${kategoriParam}`,
+                    `https://e-library.up.railway.app/api/Buku/KoleksiBuku?usia=dewasa&page=${page}&per_page=${this.perPage}${kategoriParam}`,
                     {
                         headers: {
                             Authorization: `Bearer ${sessionStorage.getItem('token')}`,
@@ -226,7 +226,7 @@ export default {
         },
         loadKategori() {
             axios
-                .get('https://laravel-library-production.up.railway.app/api/Buku/DaftarKategori', {
+                .get('https://e-library.up.railway.app/api/Buku/DaftarKategori', {
                     headers: { Authorization: `Bearer ${this.token}` },
                 })
                 .then((responseKategori) => {
@@ -236,7 +236,7 @@ export default {
 
                     axios
                         .get(
-                            `https://laravel-library-production.up.railway.app/api/Buku/KoleksiBuku?usia=dewasa&per_page=${this.perPage}`,
+                            `https://e-library.up.railway.app/api/Buku/KoleksiBuku?usia=dewasa&per_page=${this.perPage}`,
                             {
                                 headers: { Authorization: `Bearer ${this.token}` },
                             },
@@ -292,7 +292,7 @@ export default {
             this.isSearching = true
             axios
                 .get(
-                    `https://laravel-library-production.up.railway.app/api/Buku/KoleksiBuku?usia=dewasa&per_page=${this.perPage}`,
+                    `https://e-library.up.railway.app/api/Buku/KoleksiBuku?usia=dewasa&per_page=${this.perPage}`,
                     {
                         headers: { Authorization: `Bearer ${this.token}` },
                     },
